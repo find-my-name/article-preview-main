9a8ad1b570dd6043a5932c7637a28d220061b5ef
Mike <mvalan27@gmail.com>
2024-01-07 17:39:00 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const displayShareLinks = () => {
+  document.querySelector(".share-platforms").classList.toggle("hidden");
+  document.querySelector(".share-btn").classList.toggle("active");
+  document.querySelector(".btn").classList.toggle("active");
+};
